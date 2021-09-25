0b1b0df126eb53d4111129492fe8059027f3939d
Smitty (via script) <me@smitop.com>
2021-09-25 17:46:01 +0000

    Add user ID to SE post links
    This commit was automatically generated from 2efc9ebecc35a01453642b33a8830424c534d790

--- build-data/get/se.js
+++ build-data/get/se.js
@@ -24,7 +24,7 @@ const fetch = require("node-fetch");
                 Type: "SePost",
                 Id: `se-${siteId}-${post.post_id}`,
                 Title: post.title,
-                Uri: post.link,
+                Uri: post.link + "/14004262", // append user ID to get tracking right
                 Date: { Unix: post.creation_date },
                 Text: post.body_markdown,
                 Verbose: 1,

7c1b9f6004edf98f02bfe0e552ae2b1cbb338fad
Smitty (via script) <me@smitop.com>
2021-09-29 19:48:36 +0000

    smitop.com/segme
    This commit was automatically generated from 37087744f16cf2976356eeb0a02a713953b4ee87

--- subrepos.sh
+++ subrepos.sh
@@ -1,5 +1,16 @@
 cd static
+
 rm -rf transform-playground
 git clone https://github.com/Smittyvb/transform-playground.git
 rm -rf transform-playground/.git
+
+rm -rf segme segmerepo
+git clone https://github.com/Smittyvb/segme.git segmerepo
+cd segmerepo
+wasm-pack build -t no-modules
+mv pkg public
+cd ..
+mv segmerepo/public segme
+rm -rf segmerepo
+
 cd ..

5830985321cf731188aa8c4d1416acd4801e1515
Smitty (via script) <me@smitop.com>
2023-11-23 18:36:56 +0000

    Update nix config
    This commit was automatically generated from 14725406e78d4c3698343210ec609da8e42ae803

--- conf/nix/base.nix
+++ conf/nix/base.nix
@@ -1,44 +1,32 @@
-{ config, pkgs, ... }:
+# Should be imported into each system file
+
+{ config, lib, pkgs, modulesPath, ... }:
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       /etc/nixos/hardware-configuration.nix
     ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/Toronto";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_CA.UTF-8";
 
-  # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
-  # Configure keymap in X11
   services.xserver = {
     layout = "us";
     xkbVariant = "colemak";
   };
   console.useXkbConfig = true;
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  services.printing.enable = true; # Enable CUPS to print documents
 
-  # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -55,10 +43,6 @@
     #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.loops = {
     isNormalUser = true;
     description = "Smitty";
@@ -70,15 +54,12 @@
       gcc
       pkg-config
       openssl
-    #  thunderbird
+      imagemagick
     ];
   };
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     git
     wget
@@ -105,12 +86,4 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
 }
---
+++ conf/nix/tadbulb.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, ... }:
+{
+  imports = [ ./base.nix ];
+
+  networking.hostName = "tadbulb";
+
+  # stateVersion - do not change without reading docs!
+  system.stateVersion = "23.05"; # Did you read the comment?
+}

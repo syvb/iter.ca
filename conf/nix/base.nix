f9badd45e88668f3204793641f8fc0c2b33099cb
Smitty (via script) <me@smitop.com>
2023-12-19 23:56:26 +0000

    enable MTR
    This commit was automatically generated from 37e1ed23827999dceababbbd4944338e02343f75

--- conf/nix/base.nix
+++ conf/nix/base.nix
@@ -71,18 +71,9 @@
     gh
     jq
   ];
+  programs.mtr.enable = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];

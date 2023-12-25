6991155d613be215c0318fcb9b15556f83a33108
Smitty (via script) <me@smitop.com>
2023-12-25 04:06:23 +0000

    Use proper way to invoke Bash for NixOS
    This commit was automatically generated from 7bb72e0817dc10af398e4a235592241ad01e37b1

--- build-data/get/get-thumbs.sh
+++ build-data/get/get-thumbs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 >&2 echo "Getting YouTube thumbnails"
 while read -r id
--- regen.sh
+++ regen.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 cd build-data/get || exit
 ./all.sh > ../../data/data.json
 cd ../.. || exit

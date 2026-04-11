3de5edcc957ca37213d7af95c1dcab343574e1a9
Smitty (via script) <me@smitop.com>
2026-04-11 04:04:38 +0000

    compress image
    This commit was automatically generated from 3457f730d91b95e0fe4a7ae412abcea312f368c7

--- subrepos.sh
+++ subrepos.sh
@@ -35,7 +35,9 @@ rm -rf segmerepo
     wget 'https://archive.org/download/lifelogging-2026-03-31/PXL_20260401_032801897.RAW-01.COVER.jpg'
     wget 'https://archive.org/download/lifelogging-2026-03-31/PXL_20260401_032815315.RAW-01.COVER.jpg'
     wget 'https://archive.org/download/lifelogging-2026-03-31/PXL_20260401_032826782.RAW-01.COVER.jpg'
-    wget -O bad-math-poster.jpg 'https://www.flickr.com/photo_download.gne?size=o&id=55190563458&secret=29ceebc973'
+    wget -O bad-math-poster-orig.jpg 'https://www.flickr.com/photo_download.gne?size=o&id=55190563458&secret=29ceebc973'
+    magick bad-math-poster-orig.jpg -define jpeg:extent=1MB bad-math-poster.jpg
+    rm bad-math-poster-orig.jpg
 )
 
 rm -rf fmat
